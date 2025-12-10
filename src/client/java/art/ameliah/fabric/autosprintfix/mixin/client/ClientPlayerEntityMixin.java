cd6921ca270936822990fa8de31db75071f2fd08
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-12-10 21:06:50 +0100

    1.21.11 support

--- src/client/java/art/ameliah/fabric/autosprintfix/mixin/client/ClientPlayerEntityMixin.java
+++ src/client/java/art/ameliah/fabric/autosprintfix/mixin/client/ClientPlayerEntityMixin.java
@@ -1,27 +1,21 @@
 package art.ameliah.fabric.autosprintfix.mixin.client;
 
 import art.ameliah.fabric.autosprintfix.AutoSprintFixClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.network.packet.c2s.play.ClientStatusC2SPacket;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerEntity.class)
+@Mixin(LocalPlayer.class)
 public class ClientPlayerEntityMixin {
 
-    @Shadow @Final public ClientPlayNetworkHandler networkHandler;
-
-    @Inject(at = @At("HEAD"), method = "requestRespawn")
+    @Inject(at = @At("HEAD"), method = "respawn")
     private void requestRespawnHead(CallbackInfo ci) {
         AutoSprintFixClient.respawning = true;
     }
 
-    @Inject(at = @At("TAIL"), method = "requestRespawn")
+    @Inject(at = @At("TAIL"), method = "respawn")
     private void requestRespawnTail(CallbackInfo ci) {
         AutoSprintFixClient.respawning = false;
     }
--- src/client/java/art/ameliah/fabric/autosprintfix/mixin/client/StickyKeyBindingMixin.java
+++ src/client/java/art/ameliah/fabric/autosprintfix/mixin/client/StickyKeyBindingMixin.java
@@ -1,16 +1,16 @@
 package art.ameliah.fabric.autosprintfix.mixin.client;
 
 import art.ameliah.fabric.autosprintfix.AutoSprintFixClient;
-import net.minecraft.client.option.StickyKeyBinding;
+import net.minecraft.client.ToggleKeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(StickyKeyBinding.class)
+@Mixin(ToggleKeyMapping.class)
 public class StickyKeyBindingMixin {
 
-    @Inject(at = @At("HEAD"), method = "untoggle", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "reset", cancellable = true)
     public void onUnToggle(CallbackInfo ci) {
         if (AutoSprintFixClient.respawning) {
             ci.cancel();
